7d5f36aa5be1b3cc24c28b933daff5642beaeb52
Michał Hoffmann <michalhoffmann@Michas-MacBook-Pro-2.local>
2024-06-09 20:38:29 +0200

    draft for cnn

--- code/layers.py
+++ code/layers.py
@@ -1,11 +1,46 @@
 import numpy as np
 
-class ConvLayer:
-    def __init__(self, num_filters, filter_size, stride, padding):
+class Layer:
+    def __init__(self, input_shape):
+        self.activation = None
+        self.delta = None
+        self.id = None
+        
+        self.input_shape = input_shape
+    
+    def set_id(self, id):
+        self.id = id
+    
+    def forward(self, X):
+        pass
+    
+    def backward(self, delta):
+        pass
+    
+    def set_id(self, id):
+        self.id = id
+        
+    def get_output(self):
+        return self.output
+
+class ConvLayer(Layer):
+    def __init__(self, num_filters: int, filter_size: tuple, stride, padding, input_shape):
+        '''
+        do we want stride and padding?
+        
+        do we want input_shape?
+        
+        do we want to initialize the weights here?
+        
+        do we want to initialize the biases here?
+        
+        do we want to initialize the activation here?
+        '''
         self.num_filters = num_filters
         self.filter_size = filter_size
         self.stride = stride
         self.padding = padding
+        self.input_shape = input_shape
     
     def forward(self, X):
         pass
@@ -15,7 +50,7 @@ class ConvLayer:
         pass
     
         
-class PoolLayer:
+class PoolLayer(Layer):
     def __init__(self, pool_size, stride):
         self.pool_size = pool_size
         self.stride = stride
@@ -26,26 +61,16 @@ class PoolLayer:
     def backward(self, delta):
         pass
     
-class FlattenLayer:
+class FlattenLayer(Layer):
     def forward(self, X):
         pass
     
     def backward(self, delta):
         pass
     
-class DenseLayer:
+class DenseLayer(Layer):
     def __init__(self, num_neurons):
         self.num_neurons = num_neurons
-    
-    def forward(self, X):
-        pass
-    
-    def backward(self, delta):
-        pass
-    
-class ActivationLayer:
-    def __init__(self, activation):
-        self.activation = activation
     
     def forward(self, X):
         pass
