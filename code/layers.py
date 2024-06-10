8141994d109a3ae1d74c35cc3c64b1c408b093c9
Marwyk2003 <marwyk2003@gmail.com>
2024-06-10 19:47:32 +0200

    add batches to conv layer
    fix activation derrivatives

--- code/layers.py
+++ code/layers.py
@@ -35,37 +35,6 @@ class Layer:
     def bias(self):
         pass
 
-class ConvLayer(Layer):
-    def __init__(self, num_kernels: int, kernel_size: int, input_shape: tuple, activation='relu'):
-        """
-        CNN - layer
-        Based on: https://www.youtube.com/watch?v=Lakz2MoHy6o
-        """
-
-        height, width, channels = input_shape
-        self.num_kernels = num_kernels  # K
-        self.input_shape = input_shape  # (H, W, C)
-        self.num_channels = channels  # C
-        self.output_shape = (num_kernels, height - kernel_size + 1, width - kernel_size + 1)  # (K, H-KS+1, W+KS+1)
-        self.kernels_shape = (num_kernels, channels, kernel_size, kernel_size)  # (K, C, KS, KS)
-        self.kernels = np.random.randn(*self.kernels_shape)  # (K, C, KS, KS)
-        self.biases = np.random.randn(*self.output_shape)  # (K, H, W)
-
-        self.kernels_grad = np.zeros(self.kernels_shape)
-        self.biases_grad = np.zeros(self.output_shape)
-
-        self.activation = activation
-
-    def forward(self, X):
-        self.input = X  # (H, W, C)
-        self.output = np.copy(self.biases)
-        for i in range(self.num_kernels):
-            for j in range(self.num_channels):
-                # print(self.input[:, :, j].shape, self.kernels[i].shape)
-                self.output[i] += signal.correlate2d(self.input[:, :, j], self.kernels[i, j], mode='valid')
-        self.derivative = self.activate_derivative(self.output)
-        return self.activate(self.output)
-    
     def activate(self, X):
         if self.activation == 'relu':
             return np.maximum(X, 0)
@@ -75,7 +44,7 @@ class ConvLayer(Layer):
             return np.tanh(X)
         else:
             return X
-        
+
     def activate_derivative(self, X):
         '''
         Derivative of the activation function
@@ -84,11 +53,49 @@ class ConvLayer(Layer):
         if self.activation == 'relu':
             return np.where(X > 0, 1, 0)
         elif self.activation == 'sigmoid':
-            return X * (1 - X)
+            sX = 1 / 1 + np.exp(-X)
+            return sX * (1 - sX)
         elif self.activation == 'tanh':
-            return 1 - X ** 2
+            tanhX = np.tanh(X)
+            return 1 - tanhX ** 2
         else:
             return X
+
+
+class ConvLayer(Layer):
+    def __init__(self, num_kernels: int, kernel_size: int, input_shape: tuple, activation='relu'):
+        """
+        CNN - layer
+        Based on: https://www.youtube.com/watch?v=Lakz2MoHy6o
+        """
+
+        num_batches, height, width, channels = input_shape
+        self.batch_num = num_batches
+        self.num_kernels = num_kernels  # K
+        self.input_shape = input_shape  # (B, H, W, C)
+        self.num_channels = channels  # C
+        self.output_shape = (num_batches, num_kernels, height - kernel_size + 1, width - kernel_size + 1)  # (B, K, H-KS+1, W+KS+1)
+
+        self.kernels_shape = (num_kernels, channels, kernel_size, kernel_size)  # (K, C, KS, KS)
+        self.kernels = np.random.randn(*self.kernels_shape)  # (K, C, KS, KS)
+        self.biases = np.random.randn(*self.output_shape[1:])  # (K, H, W)
+
+        self.kernels_grad = np.zeros(self.kernels_shape)
+        self.biases_grad = np.zeros(self.output_shape[1:])
+
+        self.activation = activation
+
+    def forward(self, X):
+        self.input = X  # (H, W, C)
+        self.output = np.ndarray(self.output_shape)
+        for b in range(self.batch_num):
+            self.output[b] = np.copy(self.biases)
+            for i in range(self.num_kernels):
+                for j in range(self.num_channels):
+                    # print(self.input[:, :, j].shape, self.kernels[i].shape)
+                    self.output[b, i] += signal.correlate2d(self.input[b, :, :, j], self.kernels[i, j], mode='valid')
+        self.derivative = self.activate_derivative(self.output)
+        return self.activate(self.output)
 
     def backward(self, output_grad):
         output_grad = output_grad * self.derivative
@@ -119,6 +126,7 @@ class ConvLayer(Layer):
     def bias(self):
         return self.biases
 
+
 class PoolLayer(Layer):
     def __init__(self, num_filters: int, filter_size: tuple, stride, padding, input_shape):
         self.num_filters = num_filters
@@ -126,10 +134,10 @@ class PoolLayer(Layer):
         self.stride = stride
         self.padding = padding
         self.input_shape = input_shape
-    
-        
+
+
 class MaxPoolLayer(Layer):
-    def __init__(self, pool_size, input_shape, stride = None, pool_type='max'):
+    def __init__(self, pool_size, input_shape, stride=None, pool_type='max'):
         self.pool_size = pool_size
         if stride is None:
             self.stride = pool_size
@@ -137,46 +145,51 @@ class MaxPoolLayer(Layer):
             self.stride = stride
         self.input_shape = input_shape
         self.pool_type = pool_type
-        self.output_shape = (input_shape[0], (input_shape[1] - self.pool_size[0]) // self.stride[0] + 1, (input_shape[2] - self.pool_size[1]) // self.stride[1] + 1, input_shape[3])
-        
+        self.output_shape = (input_shape[0], (input_shape[1] - self.pool_size[0]) // self.stride[0] + 1,
+                             (input_shape[2] - self.pool_size[1]) // self.stride[1] + 1, input_shape[3])
+
     def _compress_indices(self):
-        return np.mean(self.indices, axis=0)    
-    
+        return np.mean(self.indices, axis=0)
+
     def forward(self, X):
         # implement max pooling and save the indices of the max values
         print(X.shape)
         self.output = np.zeros(self.output_shape)
-        
+
         (batch, x, y, channel) = X.shape
-        
+
         self.indices = np.zeros(self.input_shape)
-        
+
         for i in range(0, x - self.pool_size[0] + 1, self.stride[0]):
             for j in range(0, y - self.pool_size[1] + 1, self.stride[1]):
                 for c in range(channel):
-                    self.output[:, i // self.stride[0], j // self.stride[1], c] = np.max(X[:, i:i+self.pool_size[0], j:j+self.pool_size[1], c], axis=(1, 2))
-                    self.indices[:, i:i+self.pool_size[0], j:j+self.pool_size[1], c] = np.where(X[:, i:i+self.pool_size[0], j:j+self.pool_size[1], c] == self.output[:, i // self.stride[0], j // self.stride[1], c].reshape(-1, 1, 1), 1, 0)
-                    
+                    self.output[:, i // self.stride[0], j // self.stride[1], c] = np.max(
+                        X[:, i:i + self.pool_size[0], j:j + self.pool_size[1], c], axis=(1, 2))
+                    self.indices[:, i:i + self.pool_size[0], j:j + self.pool_size[1], c] = np.where(
+                        X[:, i:i + self.pool_size[0], j:j + self.pool_size[1], c] == self.output[:, i // self.stride[0],
+                                                                                     j // self.stride[1], c].reshape(-1,
+                                                                                                                     1,
+                                                                                                                     1),
+                        1, 0)
+
         self.indices = self._compress_indices()
         print(self.indices.shape)
 
         return self.output
-    
+
     def backward(self, delta):
         # implement backpropagation for max pooling using the saved indices
-        
-        delta_out = np.zeros(self.input_shape)
-        
         delta = np.repeat(delta, self.pool_size[0], axis=0)
-        
+
         delta = np.repeat(delta, self.pool_size[1], axis=1)
-        
+
         delta_out = delta * self.indices
-        
+
         return delta_out
-    
+
+
 class MeanPoolLayer(Layer):
-    def __init__(self, pool_size, input_shape, stride = None, pool_type='max'):
+    def __init__(self, pool_size, input_shape, stride=None, pool_type='max'):
         self.pool_size = pool_size
         if stride is None:
             self.stride = pool_size
@@ -184,78 +197,55 @@ class MeanPoolLayer(Layer):
             self.stride = stride
         self.input_shape = input_shape
         self.pool_type = pool_type
-        
-        self.output_shape = (input_shape[0], (input_shape[1] - self.pool_size[0]) // self.stride[0] + 1, (input_shape[2] - self.pool_size[1]) // self.stride[1] + 1, input_shape[3])
-    
+
+        self.output_shape = (input_shape[0], (input_shape[1] - self.pool_size[0]) // self.stride[0] + 1,
+                             (input_shape[2] - self.pool_size[1]) // self.stride[1] + 1, input_shape[3])
+
     def forward(self, X):
         # implement mean pooling
-        self.output = np.zeros(self.output_shape)        
-        
-        reshaped_input = X.reshape(X.shape[0], X.shape[1] // self.pool_size[0], self.pool_size[0], X.shape[2] // self.pool_size[1], self.pool_size[1], X.shape[3])
+        self.output = np.zeros(self.output_shape)
+
+        reshaped_input = X.reshape(X.shape[0], X.shape[1] // self.pool_size[0], self.pool_size[0],
+                                   X.shape[2] // self.pool_size[1], self.pool_size[1], X.shape[3])
         output = reshaped_input.mean(axis=(2, 4))
         reshaped_input = reshaped_input.transpose(0, 1, 3, 5, 2, 4)
         # self.indices = each neuron equal weight
-        return self.activate(output)        
-        
-                        
-    
+        return self.activate(output)
+
     def backward(self, output_grad):
         # implement backpropagation for mean pooling
 
         output_grad = np.repeat(output_grad, self.pool_size[0], axis=0)
         output_grad = np.repeat(output_grad, self.pool_size[1], axis=1)
-        
+
         output_grad = output_grad / (self.pool_size[0] * self.pool_size[1])
 
         return output_grad
-    
+
+
 class FlattenLayer(Layer):
     def __init__(self, input_shape):
         self.input_shape = input_shape
         self.output_shape = (input_shape[0], np.prod(input_shape[1:]))
-        
+
     def forward(self, X):
         self.input = X
         self.output = X.reshape(X.shape[0], -1)
-        
+
         return self.output
-        
+
     def backward(self, output_grad):
         return output_grad.reshape(self.input_shape)
+
 
 class DenseLayer(Layer):
     def __init__(self, units, input_shape, activation='relu'):
         self.units = units
         self.input_shape = input_shape
         self.activation = activation
-        
+
         self.weights = np.random.randn(input_shape[1], units) - 0.5
         self.biases = np.random.randn(units) - 0.5
-
-    def activate(self, X):
-        if self.activation == 'relu':
-            return np.maximum(X, 0)
-        elif self.activation == 'sigmoid':
-            return 1 / (1 + np.exp(-X))
-        elif self.activation == 'tanh':
-            return np.tanh(X)
-        else:
-            return X
-        
-                
-    def activate_derivative(self, X):
-        '''
-        Derivative of the activation function
-        X: input to the activation function
-        '''
-        if self.activation == 'relu':
-            return np.where(X > 0, 1, 0)
-        elif self.activation == 'sigmoid':
-            return X * (1 - X)
-        elif self.activation == 'tanh':
-            return 1 - X ** 2
-        else:
-            return X
 
     def forward(self, X):
         self.input = X
